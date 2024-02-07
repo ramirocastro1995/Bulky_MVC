0bd5c21fd0c93321cebea9df9859fec97148d0cf
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-02-07 18:40:16 -0300

    -Category controllers/model/view -Modification of html styling and adding bootstrap theme

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace BulkyWeb.Models
+{
+    /// <summary>
+    /// Category is the table that we want to create in EF,with id/name/display rows
+    /// </summary>
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
