dfc2bd81a96eecb1158ef681be9fe187187e3283
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-02-13 14:14:23 -0300

    Server Side Validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -35,9 +35,11 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (ModelState.IsValid) { 
             _db.Categories.Add(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+            }
+            return View();
         }
 
 
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -10,9 +10,11 @@ namespace BulkyWeb.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display order must be between 1 - 100")]
         public int DisplayOrder { get; set; }
     }
 }
