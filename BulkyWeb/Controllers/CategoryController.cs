4ba51bdae914d2f4d4ce5510e23a07e08aff2b36
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-02-20 15:35:09 -0300

    -Add ValidationScripts -Add Edit and Delete first part

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -36,13 +36,44 @@ namespace BulkyWeb.Controllers
         public IActionResult Create(Category obj)
         {
             //Optional special validation
-            //if(obj.Name.ToLower() == obj.DisplayOrder.ToString().ToLower()) 
-            //{
-            //    ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
-            //}
+            if(obj.Name.ToLower() == obj.DisplayOrder.ToString().ToLower()) 
+            {
+                ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
+            }
             if (ModelState.IsValid) { 
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //To use the post method
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //Optional special validation
+            if (obj.Name.ToLower() == obj.DisplayOrder.ToString().ToLower())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
             }
             return View();
         }
