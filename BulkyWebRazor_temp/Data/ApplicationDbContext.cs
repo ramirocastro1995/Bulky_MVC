8c63c8910058f2eeaf26cdb64db5ccd6987fd503
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-02-23 14:30:30 -0300

    -Add temp data -Add temp data to partial view -Add toastr -RazorPages test app

---
+++ BulkyWebRazor_temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWebRazor_temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Terror", DisplayOrder = 1 } 
+                );
+        }
+    }
+}
