dc92397cfc3a6d0be0fbee2c7246c5fc432bf4b1
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-04-04 18:14:37 -0300

    -End of Role implementation -Authorization -Add Company Model

---
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,167 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+ using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json.Serialization;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnviroment;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            
+
+        }
+        public IActionResult Index()
+        {
+            //Get everything to show in index
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            
+            return View(objCompanyList);
+
+        }
+
+        //Upsert -> update and insert
+        //Id id exists ->update
+        public IActionResult Upsert(int? id)
+        {
+
+            //ViewBag.CategoryList = categoryList;
+            
+            
+            //If id is null or cero the Company dosn't exists,so create new
+            if (id==null || id == 0)
+            {
+                //create
+                return View(new Company());
+
+            }
+            //if id exists -> update
+            else 
+            {
+                //Update
+                Company companyObj = _unitOfWork.Company.Get(x => x.Id == id);
+                return View(companyObj);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company CompanyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+                if (CompanyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(CompanyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(CompanyObj);
+                }
+                _unitOfWork.Save();
+                TempData["success"] = "Company created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //If is not valid it populates the category drop down
+                
+                return View(CompanyObj);
+            }
+
+        }
+
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Company? CompanyFromDb = _unitOfWork.Company.Get(x => id == x.Id);
+        //    if (CompanyFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(CompanyFromDb);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(Company obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Company.Update(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Category Updated successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+
+        //public IActionResult Delete(int? id)
+        //{
+        //    if(id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Company? obj = _unitOfWork.Company.Get(x => id == x.Id);
+
+        //    if(obj == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(obj);
+
+        //}
+
+        //[HttpPost,ActionName("Delete")]
+        //public IActionResult DeletePOST(Company obj)
+        //{
+
+        //    _unitOfWork.Company.Remove(obj);
+        //    _unitOfWork.Save();
+        //    TempData["success"] = "Category deleted successfully";
+        //    return RedirectToAction("Index");
+
+
+
+        //}
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompanyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var CompanyToBeDeleted = _unitOfWork.Company.Get(x => x.Id == id);
+
+            if (CompanyToBeDeleted == null)
+            {
+                return Json(new {success=false, message = "Error while deleting" });
+            }
+            _unitOfWork.Company.Remove(CompanyToBeDeleted);
+            _unitOfWork.Save();
+            return Json(new { success = true,message = "Delete successfull"});
+        }
+        #endregion
+
+    }
+}
