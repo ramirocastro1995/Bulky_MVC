ce534262f5218eade28fb9a77bfadf8fa81be3b5
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-04-26 10:57:07 -0300

    -Add Stripe keys/ Package -Add "paid" logic -Add OrderController -Add OrderVM

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,7 +4,9 @@ using Bulky.Models.ViewModels;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.BillingPortal;
 using Stripe.Checkout;
+using Stripe.FinancialConnections;
 using System.Drawing.Text;
 using System.Security.Claims;
 
@@ -126,7 +128,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
 			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
 			{
-				var domain= "https://localhost:7169/";
+				var domain= "https://localhost:7121/";
 				var options = new Stripe.Checkout.SessionCreateOptions
 				{
 					SuccessUrl =domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
@@ -153,7 +155,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 				options.LineItems.Add(sessionLineItem);
 				}
 				var service = new Stripe.Checkout.SessionService();
-				Session session = service.Create(options);
+                Stripe.Checkout.Session session = service.Create(options);
 				_unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
 				_unitOfWork.Save();
 				Response.Headers.Add("Location", session.Url);
@@ -167,6 +169,27 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
 		public IActionResult OrderConfirmation(int id)
 		{
+			OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(x => x.Id == id,includeProperties: "ApplicationUser");
+			if(orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+			{
+				//SessionService from stripe package
+				var service = new Stripe.Checkout.SessionService();
+				Stripe.Checkout.Session session = service.Get(orderHeader.SessionId);
+
+				if(session.PaymentStatus.ToLower() == "paid") 
+				{
+					_unitOfWork.OrderHeader.UpdateStripePaymentID(id,session.Id, session.PaymentIntentId);
+					_unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+					_unitOfWork.Save();
+				}
+
+            }
+
+			List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll
+				(x => x.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+			_unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+			_unitOfWork.Save();
+
 			return View(id);
 		}
 
